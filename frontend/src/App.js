999181d38b77042847a211d3eaa8739aadcba5f6
Gokul Vinayagam <gokulvinayagam92@gmail.com>
2025-01-12 21:01:41 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,23 +9,23 @@ import "./App.css";
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route
-                path="/school-transactions"
-                element={<SchoolTransactions />}
-              />
-              <Route path="/status-check" element={<StatusCheck />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
-    </ThemeProvider>
+<ThemeProvider>
+<Router>
+<div className="app">
+<Navbar />
+<div className="container">
+<Routes>
+<Route path="/" element={<Dashboard />} />
+<Route
+path="/school-transactions"
+element={<SchoolTransactions />}
+/>
+<Route path="/status-check" element={<StatusCheck />} />
+</Routes>
+</div>
+</div>
+</Router>
+</ThemeProvider>
   );
 }
 
